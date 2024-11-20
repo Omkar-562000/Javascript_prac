4509f96a6201dcf9c8be48dd7d55529f14abe0be
Omkar <omkarghanekar56@email.com>
2024-11-20 21:12:23 +0530

    commit

---
+++ 03_javascriptconversionofdatatype.js
@@ -0,0 +1,12 @@
+let marks = "omkar"
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(marks)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" => 33
+// "33abc" => NaN
+//true => 1; false =>0
+
